b65c900cfc51602a67c36586e76f5dc35b4399cf
Dellamoresteven <dellamoresteven@gmail.com>
2020-01-30 18:24:56 -0500

    token stuff

--- Frontend/p5/Teamstris/debug.js
+++ Frontend/p5/Teamstris/debug.js
@@ -1,7 +1,9 @@
 var startscreen_constructor = false;
 var startscreen_draw        = false;
+var startscreen_mouseClickedStart = false;
 
 var buttons_constructor     = true;
 var buttons_draw            = true;
 
 var buttons_checkmouse      = false;
+
