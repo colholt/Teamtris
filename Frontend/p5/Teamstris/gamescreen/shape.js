c5bf73377ca2a02d3cd3969919e29bf467cde197
Richard Hansengit config --global user.name Richard <richie.hansen@gmail.com>
2020-02-27 17:46:11 -0500

    please

--- Frontend/p5/Teamstris/gamescreen/shape.js
+++ Frontend/p5/Teamstris/gamescreen/shape.js
@@ -68,7 +68,7 @@ class Shape {
 	RandomColor() {
 		var Colors = ["red","blue","green","yellow"]
 		return Colors[this.ID-1]
-		return Colors[Math.floor(Math.random()*Colors.length)]
+		//return Colors[Math.floor(Math.random()*Colors.length)]
 	}
 
 	/** 
