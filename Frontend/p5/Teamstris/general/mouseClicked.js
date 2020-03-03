e8b93ce8bd832302f690b510ea681dacdf836f3d
Dellamoresteven <dellamoresteven@gmail.com>
2020-03-03 18:29:04 -0500

    More comments

--- Frontend/p5/Teamstris/general/mouseClicked.js
+++ Frontend/p5/Teamstris/general/mouseClicked.js
@@ -1,16 +1,35 @@
+
+// #class General |
+// @author Steven Dellamore, Richard Hansen |
+// @desc This is an abstract class that will hold mouseClicked and 
+// keyPressed p5 functions. |
+
+// #function General::mouseClicked |
+// @author Steven Dellamore, Richard Hansen |
+// @desc Will be called whenever the user clicks on anywhere on the screen.
+// Once called, it will go straight into a switch to decide where to 
+// route to based on the gameState
+// @link{mouseClickedGeneralVar1} 
+// The varibles @inline{gameState}, @inline{mStartScreen},
+// @inline{mLobbyScreen} are all defined in sketch.js | 
+// @param void : takes no parameters |
+// @returns void : returns nothing |
+// @header mouseClicked() |
 function mouseClicked() {
+    // #code mouseClickedGeneralVar1 javascript
     switch (gameState) {
         case 0:
-            mStartScreen.mouseClickedStart();
+            // start screens mouseClicked
+            mStartScreen.mouseClickedStart(); 
             break;
         case 1:
+            // lobby screens mouseClicked
             mLobbyScreen.mouseClickedLobby();
             break;
         case 2:
-
             break;
         case 3:
-
             break;
     }
+    // |
 }
