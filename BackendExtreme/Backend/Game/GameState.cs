ed455522e503638a2feea0dce425e48e24e57df2
colholt <mmomain@gmail.com>
2020-04-02 21:00:20 -0400

    update per second

--- BackendExtreme/Backend/Game/GameState.cs
+++ BackendExtreme/Backend/Game/GameState.cs
@@ -6,7 +6,7 @@ public class GameState
 
     public long clock = 0;
     public int start_time;
-    public int current_time;
+    public int current_time = 0;
     public int type = 100;
     public Dictionary<int, Player> players;
     public Bot bot;
