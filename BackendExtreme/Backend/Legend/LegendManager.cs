fc49bf449de6c7397055fdba5b29b8aa45abc45a
30945803485 <30945803485@users.noreply.github.com>
2020-04-24 13:14:33 -0400

    legend setup

---
+++ BackendExtreme/Backend/Legend/LegendManager.cs
@@ -0,0 +1,37 @@
+using System;
+using WebSocketSharp;
+using WebSocketSharp.Server;
+using System.Threading;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+public class LegendManager : WebSocketBehavior
+{
+    private Thread thread;
+    private int count;
+    private string bean;
+
+    public LegendManager(){}
+
+    protected override void OnOpen()
+    {
+        Console.WriteLine("legend info recieved");
+    }
+
+    protected override void OnMessage(MessageEventArgs e)
+    {
+        Console.WriteLine(e.Data);
+        Packet packet = JsonConvert.DeserializeObject<Packet>(e.Data);
+        string socketID = ID;
+
+        // create the legend and send back
+        CreateLegend();
+    }
+
+    public void CreateLegend() {
+        
+    }
+ 
+}
+
+    
